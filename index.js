b8111d334c9633ae601ec43aa4635b15302f6043
aleivc <aleivc@outlook.com>
2024-03-06 16:30:40 +0800

    refactor code.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const WebSocket = require('ws');
 const mqtt = require('mqtt');
 const {MongoClient} = require('mongodb');
@@ -6,14 +7,6 @@ const {MongoClient} = require('mongodb');
 const port = 3001;
 const wss = new WebSocket.Server({port}, () => console.log('server is listening on ', port))
 const clients = [];
-
-function sendToAll(message) {
-    clients.forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(message);
-        }
-    });
-}
 
 wss.on('connection', function connection(ws) {
     clients.push(ws);
@@ -50,37 +43,43 @@ async function connectToMongo() {
 
 let mongoCollection;
 
-connectToMongo()
-    .then(collection => {
-        mongoCollection = collection;
-        startMqtt();
-    })
-    .catch(error => {
-        console.error('Error connecting to MongoDB:', error);
-    });
+// connectToMongo()
+//     .then(collection => {
+//         mongoCollection = collection;
+//     })
+//     .catch(error => {
+//         console.error('Error connecting to MongoDB:', error);
+//     });
 
-const topic = 'sensor/001/temp012'
-const clientId = "emqx_react_" + Math.random().toString(16).substring(2, 8);
+const config = ['sensor/001', 'sensor/002', 'sensor/003'];
+const clientId = "id_" + Math.random().toString(16).substring(2, 8);
+
+startMqtt();
+
 function startMqtt() {
     const client = mqtt.connect(`ws://${MQTT_BROKER_HOST}:8083/mqtt`, {
         clientId,
         username: MQTT_USER_NAME,
         password: MQTT_PASS_WORD
     });
+
     client.on("connect", () => {
         console.log("Connected to MQTT broker");
-        client.subscribe(topic, (err) => {
-            if (err) {
-                console.error("Error subscribing to MQTT topic:", err);
-            } else {
-                console.log("Subscribed to MQTT topic:", topic);
-            }
-        });
+        // subscribe all topics.
+        config.forEach(topic => {
+            client.subscribe(topic, (err) => {
+                if (err) {
+                    console.error("Error subscribing to MQTT topic:", err);
+                } else {
+                    console.log("Subscribed to MQTT topic:", topic);
+                }
+            });
+        })
+
     });
 
-    client.on("message", (topic, message) => {
-        console.log(message.toString())
-        handleMessage(message);
+    client.on("message", async (topic, message) => {
+        await handleMessage(topic, message);
     });
 
     client.on('error', (error) => {
@@ -88,12 +87,18 @@ function startMqtt() {
     })
 }
 
-async function handleMessage(message) {
+async function handleMessage(topic, message) {
     try {
         const msg = JSON.parse(message.toString());
-        await mongoCollection.insertOne({...msg, timestamp: getTime()});
-        console.log("Inserted message into MongoDB:", msg);
-        sendToAll(JSON.stringify(msg))
+        // await mongoCollection.insertOne({...msg, timestamp: getTime()});
+        // console.log("Inserted message into MongoDB:", msg);
+
+        // send to all clients.
+        clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({topic, payload: msg}));
+            }
+        });
     } catch (error) {
         console.error("Error handling MQTT message:", error);
     }
